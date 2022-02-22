a78f11c1b64d329b732e221a939b43d3a12afbb8
luisa gracia <luisagmazuca@gmail.com>
2022-02-22 16:13:49 -0700

    upload first homework

---
+++ Homework01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 11:15:59 2022
+
+@author: luisa
+"""
+#Homework 1
+
+#Open file 
+file = open("C:\\Users\\luisa\\Documents\\Spring 2022\\BINF II\\Lab\\Mycobacterium_phage_Airmid.fasta", "r")
+fasta = file.read()
+
+
