41be8a2caf90b92a5da7af1d6f4bacd7c500c6f5
luisa gracia <luisagmazuca@gmail.com>
2022-02-23 20:22:56 -0700

    update

--- Homework01.py
+++ Homework01.py
@@ -23,6 +23,11 @@ import re
 file1 = open("C:\\Users\\luisa\\Documents\\Spring 2022\\BINF II\\Lab\\Mycobacterium_phage_Airmid.fasta", "r")
 fasta = file1.read()
 
+#Get phage
+pattern= re.compile(r"[a-z,M,A]{5,13}(?=\s)")
+organism= pattern.findall(fasta)
+phage = " ".join(organism)
+
 #get sequence without header
 def get_sequence(sequence):
     sequence_list= sequence.splitlines()
@@ -50,7 +55,7 @@ def get_window(sequence, start_index, window_size):
             i+=1
     return window
 
-test = get_window(comp_seq, 0, 10)
+#test = get_window(comp_seq, 0, 10)
 
 #Function to search for specific fragment inside the complete sequence
 def find_repeat(sequence, fragment):
@@ -61,22 +66,55 @@ def find_repeat(sequence, fragment):
             count +=1
     return count
 
-test2 = find_repeat(seq_list, test)
+#test2 = find_repeat(seq_list, test)
 
 #Function to find indexes
 def find_index(sequence, fragment):
+    index = 0
     for aa in sequence:
         match = re.match("(%s)" % fragment, aa)
         if match:
             index = match.span()
     return index
     
-test3 = find_index(seq_list, test)
+#test3 = find_index(seq_list, test)
 
-#functio  to create file
+#function to convert tuple to string
+def join_tuple_string(strings_tuple):
+   return ''.join(strings_tuple)
+
+
+#function  to create file
+def write_doc(data):
+    with open("file.tsv", "a+") as f:
+        f.write(data[0] + "\t" + data[1] + "\t" + data[2] + "\t" + data[3] + "\n")
 
 #Loop to call all funtions
 
+start= 0
+size= 10
+i= 0
+for i in range(len(comp_seq)):
+    repeat= get_window(comp_seq, start, size)
+    matches= str(find_repeat(seq_list, repeat))
+    indexes= find_index(seq_list, repeat)
+    
+    indexes_str = join_tuple_string(str(indexes))
+    
+    #Create list to store all variables
+    output=[]
+    output.append(phage)
+    output.append(repeat)
+    output.append(matches)
+    output.append(indexes_str)
+    
+    doc= write_doc(output)
+    
+    start +=1
+    i +=1
+    if start == len(seq_list) - size:
+        size+=1
+    
 
 
 
