3e54a87904b3e17afc6bb38ecf03c619879eaf6c
luisa gracia <luisagmazuca@gmail.com>
2022-04-07 15:55:28 -0600

    update

--- Homework07.py
+++ Homework07.py
@@ -42,14 +42,49 @@ hits_list= []
 for line in sig_hits: 
     hits_list.append(line.split())
 
-i = 0   
-for line in sig_hits:
-    globals()["hits_list" + str(i)] = []
-    #if not line.startswith("sp"):
-        #i +=1
-        #continue
-    if line.startswith("sp"):
-        globals()["hits_list" + str(i)].append(line)
+#create a list for each query
+j = 0   
+for i in range(len(sig_hits)):
+    if i == len(sig_hits)-1:
+        break
+    if not sig_hits[i].startswith("sp") and sig_hits[i+1].startswith("sp"):
+        j +=1
+        globals()["hits_list" + str(j)] = []
+    if sig_hits[i].startswith("sp"):
+        globals()["hits_list" + str(j)].append(sig_hits[i])
+        
+#make hits_lists splitliness
+def create_query (query_number):    
+    globals()["query" + str(query_number)] = []
+    for line in globals()["hits_list" + str(query_number)]: 
+        globals()["query" + str(query_number)].append(line.split())
+    return globals()["query" + str(query_number)]
+
+#call function
+query1 = create_query(1)
+query2 = create_query(2)
+query3 = create_query(3)
+query4 = create_query(4)
+
+#function to check for significant e-score value
+def check_score(query):
+    sig_hits_query = []
+    for i in range(len(query)):
+        if float(query[i][len(query[i])-1]) <= 10e-6:
+            sig_hits_query.append(query[i][1])
+            if i == 9:
+                break
+        else:
+            sig_hits_query.append(query[0][1])
+            break
+    return sig_hits_query
+
+sig_hits_query1 = check_score(query1)
+sig_hits_query2 = check_score(query2)
+sig_hits_query3 = check_score(query3)
+sig_hits_query4 = check_score(query4)
+                
+
     
         
 
@@ -61,4 +96,3 @@ for line in sig_hits:
     
     
     
-    l
